76d63eb1dd018830bbc43993ba7b997c68e77392
Aurio Rajaa <riorajaa2018@gmail.com>
2023-09-03 10:45:09 +0700

    Fixed a bug in Main.java and updated Main.class for improved performance.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class Main {
+    // MAIN METHOD
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Close (X) button
